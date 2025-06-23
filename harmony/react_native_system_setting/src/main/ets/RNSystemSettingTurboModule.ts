1a7088a440e969fb3cc28ae2c63c8294d0f2c97e
quguiren <quguiren1@huawei-partners.com>
2025-06-23 18:29:21 +0800

    add Bluetooth permission request logic
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- harmony/react_native_system_setting/src/main/ets/RNSystemSettingTurboModule.ts
+++ harmony/react_native_system_setting/src/main/ets/RNSystemSettingTurboModule.ts
@@ -1,27 +1,3 @@
-/*
- * MIT License
- *
- * Copyright (C) 2024 Huawei Device Co., Ltd.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 import { TurboModule, TurboModuleContext } from '@rnoh/react-native-openharmony/ts';
 import { TM } from "@rnoh/react-native-openharmony/generated/ts"
 import window from '@ohos.window';
@@ -31,15 +7,26 @@ import { BusinessError } from '@kit.BasicServicesKit';
 import { audio } from '@kit.AudioKit';
 import wifiManager from '@ohos.wifiManager';
 import geoLocationManager from '@ohos.geoLocationManager';
+import abilityAccessCtrl, { Permissions } from '@ohos.abilityAccessCtrl';
 
 export class RNSystemSettingTurboModule extends TurboModule implements TM.ReactNativeSystemSetting.Spec {
   constructor(protected ctx: TurboModuleContext) {
     super(ctx)
     this.setWindowClass()
+	  this.reqBluetoothPermissions()
   }
 
   private _windowClass: window.Window | undefined = undefined
   private _context: window.Window | undefined = undefined
+
+  private async reqBluetoothPermissions(): Promise<void> {
+    const permissions: Array<Permissions> = ['ohos.permission.ACCESS_BLUETOOTH'];
+    let atManager: abilityAccessCtrl.AtManager = abilityAccessCtrl.createAtManager();
+    atManager.requestPermissionsFromUser(this.ctx.uiAbilityContext, permissions).catch((err: BusinessError) => {
+      console.error(`Failed to request permissions from user. Code is ${err.code}, message is ${err.message}`);
+    })
+    return
+  }
 
   private async setWindowClass(): Promise<void> {
     let context = this.ctx.uiAbilityContext;
